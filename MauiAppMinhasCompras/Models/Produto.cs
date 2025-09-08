f17f51d6594dceef3ab52d790aa45b827a6fe9c4
MineT4 <minethiago4@gmail.com>
2025-09-07 23:53:57 -0300

    implementação do botão escluir, da edição de um item e da adição de verificadores na model

--- MauiAppMinhasCompras/Models/Produto.cs
+++ MauiAppMinhasCompras/Models/Produto.cs
@@ -4,11 +4,58 @@ namespace MauiAppMinhasCompras.Models
 {
     public class Produto
     {
+        string _descricao;
+        string _quantidade;
+        string _preco;
+
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
-        public string Descricao { get; set; }
-        public double Quantidade { get; set; }
-        public double Preco { get; set; }
+        public string Descricao
+        {
+
+            get => _descricao;
+
+            set
+            {
+                if (value == null)
+                {
+                    throw new Exception("Por favor, defina a descrição.");
+                }
+                _descricao = value;
+            }
+        }
+        public double Quantidade
+        {
+
+            get => Convert.ToDouble(_quantidade);
+
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new Exception("A quantidade deve ser maior que zero.");
+                }
+                _quantidade = value.ToString();
+            }
+        }
+        public double Preco
+        {
+
+            get => Convert.ToDouble(_preco);
+
+
+            set
+            {
+
+                if (value < 0)
+                {
+                    throw new Exception("O preço não pode ser zero.");
+                }
+                _preco = value.ToString();
+
+            }
+
+        }
         public double Total { get => Quantidade * Preco; }
 
     }
--- MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
+++ MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
@@ -15,9 +15,18 @@ public partial class ListaProduto : ContentPage
 
     protected async override void OnAppearing()
     {
-        List<Produto> tmp = await App.Db.GetAll();
+        try
+        {
+            Lista.Clear();
 
-        tmp.ForEach(i => Lista.Add(i));
+            List<Produto> tmp = await App.Db.GetAll();
+
+            tmp.ForEach(i => Lista.Add(i));
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Erro", ex.Message, "Ok");
+        }
     }
 
     private void ToolbarItem_Clicked(object sender, EventArgs e)
@@ -37,21 +46,72 @@ public partial class ListaProduto : ContentPage
 
     private async void txt_search_TextChanged(object sender, TextChangedEventArgs e)
     {
-        string P = e.NewTextValue;
+        try
+        {
 
-        Lista.Clear();
+            string P = e.NewTextValue;
 
-        List<Produto> tmp = await App.Db.Search(P);
+            Lista.Clear();
 
-        tmp.ForEach(i => Lista.Add(i));
+            List<Produto> tmp = await App.Db.Search(P);
+
+            tmp.ForEach(i => Lista.Add(i));
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Erro", ex.Message, "Ok");
+        }
     }
 
     private void ToolbarItem_Clicked_1(object sender, EventArgs e)
     {
-        double soma = Lista.Sum(i => i.Total);
+        try
+        {
+            double soma = Lista.Sum(i => i.Total);
 
-        string msg = $"O valor total da compra é {soma:C}";
+            string msg = $"O valor total da compra é {soma:C}";
 
-        DisplayAlert("Total da Compra", msg, "Ok");
+            DisplayAlert("Total da Compra", msg, "Ok");
+        }
+        catch (Exception ex)
+        {
+            DisplayAlert("Erro", ex.Message, "Ok");
+        }
+    }
+
+    private async void MenuItem_Clicked(object sender, EventArgs e)
+    {
+        try
+        {
+            MenuItem selecionado = sender as MenuItem;
+
+            Produto p = selecionado.BindingContext as Produto;
+
+            bool confirm = await DisplayAlert("Atenção", $"Confirma a exclusão do produto {p.Descricao}?", "Sim", "Não");
+
+            if (confirm)
+            {
+                await App.Db.Delete(p.Id);
+                Lista.Remove(p);
+            }
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Erro", ex.Message, "Ok");
+        }
+    }
+
+    private void lst_produtos_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+    {
+        try
+        {
+
+            Produto p = e.SelectedItem as Produto;
+            Navigation.PushAsync(new Views.EditarProduto { BindingContext = p });
+        }
+        catch (Exception ex)
+        {
+            DisplayAlert("Erro", ex.Message, "Ok");
+        }
     }
 }
